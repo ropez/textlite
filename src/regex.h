41c6ff44579e96c7179863820d392ef32f07ebdd
Robin Pedersen <robinpeder@gmail.com>
2011-10-28 21:44:06 +0200

    Docs

--- src/regex.h
+++ src/regex.h
@@ -36,15 +36,46 @@ class Regex
 public:
     typedef QString::const_iterator iterator;
 
+    /**
+      * Create an invalid regex
+      */
     Regex();
+
+    /**
+      * Compile the given pattern into a regex.
+      *
+      * If the pattern is not recognized as a regular expression, isValid()
+      * is false, and error() provides additional details.
+      */
     Regex(const QString& pattern);
+
+    /**
+      * Destruction
+      */
     ~Regex();
 
+    /**
+      * Returns true if this represents a valid regex.
+      *
+      * False if no pattern has been set, or if the pattern was not recognized.
+      * In the latter case, error() provides additional information.
+      */
     bool isValid() const;
+
+    /**
+      * If a pattern was set, and isValid() is false, an error message is
+      * available.
+      */
     QString error() const;
 
+    /**
+      * Return the pattern as a string
+      */
     QString pattern() const;
 
+    /**
+      * Returns isValid()
+      */
     bool setPattern(const QString& pattern);
 
     bool search(const QString& target, Match& match);
