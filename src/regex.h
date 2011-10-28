9a41831e0fbbbaa9d772feb9bb6d8419c1da539e
Robin Pedersen <robinpeder@gmail.com>
2011-10-28 21:53:42 +0200

    Docs

--- src/regex.h
+++ src/regex.h
@@ -6,20 +6,58 @@
 #include <QSharedPointer>
 
 class MatchPrivate;
+class RegexPrivate;
 
+/**
+  * Provides information about a successful search result.
+  */
 class Match
 {
 public:
+
+    /**
+      * Create an empty instance
+      */
     Match();
+
+    /**
+      * Destruction
+      */
     ~Match();
 
+    /**
+      * Swap the content of this object with other
+      */
     void swap(Match& other);
 
+    /**
+      * Returns true if size() is 0
+      */
     bool isEmpty() const;
+
+    /**
+      * Returns the number of matched subexpressions. 0 for newly created
+      * instances.
+      */
     int size() const;
 
+    /**
+      * Returns the position of the nth subexpression (n = 0 means the whole
+      * expression)
+      */
     int pos(int n = 0) const;
+
+    /**
+      * Returns the length of the nth subexpression (n = 0 means the whole
+      * expression)
+      */
     int len(int n = 0) const;
+
+    /**
+      * Returns the nth matched subexpression as a new string. Calling this is
+      * valid as long as the begin argument passed to Regex::search() is still
+      * valid. The returned string is a copy, which is valid forever.
+      */
     QString cap(int n = 0) const;
 
 private:
@@ -30,8 +68,9 @@ private:
     QScopedPointer<MatchPrivate> d_ptr;
 };
 
-class RegexPrivate;
-
+/**
+  * Represents a compiled regular expression.
+  */
 class Regex
 {
 public:
@@ -79,8 +118,27 @@ public:
       */
     bool setPattern(const QString& pattern);
 
+    /**
+      * Shortcut for search(target.begin(), target.end(), match)
+      */
     bool search(const QString& target, Match& match);
+
+    /**
+      * Shortcut for search(begin, end, begin, end, match)
+      */
     bool search(iterator begin, iterator end, Match& match);
+
+    /**
+      * Search for this regular expression within the given boundaries.
+      *
+      * If found, the results are stored in match, and the function returns
+      * true. If not found, the function returns false, and match is undefined.
+      *
+      * The expression must be found within the boundaries of offset and range,
+      * which must be a subregion of begin and end. The outer region, begin and
+      * end, provides the boundaries for lookbehind, lookahead, line boundaries,
+      * word boundaries and so on.
+      */
     bool search(iterator begin, iterator end, iterator offset, iterator range, Match& match);
 
 private:
