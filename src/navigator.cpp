4e729aa13e2c9aed2e1caee48e9e2110e138d418
Robin Pedersen <robinpeder@gmail.com>
2011-11-17 21:48:14 +0100

    Filename autocompletion based on Git intex
    (requires the working directory to be a Git repo)

--- src/navigator.cpp
+++ src/navigator.cpp
@@ -1,12 +1,19 @@
 #include "navigator.h"
 #include "ui_navigator.h"
 
+#include <qgitrepository.h>
+#include <qgitindex.h>
+#include <qgitindexentry.h>
+
 #include <QCompleter>
 #include <QFileSystemModel>
+#include <QStringListModel>
 #include <QLineEdit>
 #include <QDir>
 
 #include <QtDebug>
+
+using namespace LibQGit2;
 
 Navigator::Navigator(QWidget *parent) :
     QWidget(parent),
@@ -17,8 +24,25 @@ Navigator::Navigator(QWidget *parent) :
     connect(ui->comboBox, SIGNAL(returnPressed()), this, SLOT(activate()));
 
     QCompleter* completer = new QCompleter(this);
-    QFileSystemModel* model = new QFileSystemModel(completer);
-    model->setRootPath(QDir::currentPath());
+//    QFileSystemModel* model = new QFileSystemModel(completer);
+//    model->setRootPath(QDir::currentPath());
+
+    QStringList gitEntryList;
+    QGitRepository r;
+    if (r.open(".git") != 0) {
+        qWarning() << "Git open failed";
+    }
+
+    QGitIndex index(r);
+    index.read();
+
+    for (int i = 0; i < index.entryCount(); i++) {
+        QGitIndexEntry e = index.get(i);
+        gitEntryList.append(QString(e.path()));
+        qDebug() << e.id().format() << e.path();
+    }
+    QStringListModel* model = new QStringListModel(gitEntryList, this);
+
 
     completer->setModel(model);
     ui->comboBox->setCompleter(completer);
