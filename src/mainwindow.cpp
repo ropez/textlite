4c47e774d50569a5eb7bdba5b03195e2a0e68ba7
Robin Pedersen <robinpeder@gmail.com>
2011-11-26 12:37:18 +0100

    Add a toolbar with "back" and "forward" arrow buttons
    So far, no funtionality behind the buttons, and bad layout.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4,6 +4,9 @@
 #include <QApplication>
 #include <QAction>
 #include <QKeySequence>
+#include <QLabel>
+#include <QLineEdit>
+#include <QToolBar>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,10 +15,27 @@ MainWindow::MainWindow(QWidget *parent)
     setCentralWidget(win);
 
     QAction* quit = new QAction(this);
+    quit->setIcon(QIcon::fromTheme("application-exit"));
     quit->setShortcut(QKeySequence(QKeySequence::Quit));
     connect(quit, SIGNAL(triggered()), QApplication::instance(), SLOT(quit()));
     addAction(quit);
 
+    QAction* prev = new QAction(this);
+    prev->setIcon(QIcon::fromTheme("go-previous"));
+    QAction* next = new QAction(this);
+    next->setIcon(QIcon::fromTheme("go-next"));
+
+    QToolBar *tb = new QToolBar(tr("Main"), this);
+    tb->addAction(prev);
+    tb->addAction(next);
+
+//    QLineEdit* edit = new QLineEdit(this);
+//    tb->addSeparator();
+//    tb->addWidget(new QLabel(tr("File:")));
+//    tb->addWidget(edit);
+
+    tb->setMovable(false);
+    addToolBar(tb);
     resize(800, 600);
 }
 
